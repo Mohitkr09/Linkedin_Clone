e7fff9e2077e812cff17c74530c946ee57bcf6d0
Mohitkr09 <mkt27858@gmail.com>
2025-11-10 21:27:12 +0530

    ok

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -9,7 +9,6 @@ export const protect = async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
-    console.log("🔐 Token received:", token); 
   }
 
   if (!token) {
